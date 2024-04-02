76aa32da863d1e3f3df078270936606ee0849717
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-02 12:11:27 +0900

    add UnitManager : 생성자 추가

--- playGame/src/playGame/UnitManager.java
+++ playGame/src/playGame/UnitManager.java
@@ -9,4 +9,10 @@ public class UnitManager {
 	private String path = "";
 	private String[] mons = {"UnitWolf", "UnitBat", "UnitOrc"};
 	private Random ran = new Random();
+	
+	public UnitManager() {
+		player_list.add(new Player("전사", 1000, 45));
+		player_list.add(new Player("마법사", 800, 60));
+		player_list.add(new Player("힐러", 500, 70));
+	}
 }
