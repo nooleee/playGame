b860b45de938af1b6dabb5e4e577f02c4db334ec
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-02 17:53:15 +0900

    add Player : meteo 메소드 추가

--- playGame/src/playGame/Player.java
+++ playGame/src/playGame/Player.java
@@ -43,14 +43,26 @@ public class Player extends Unit {
 		}
 	}
 	
-	private void meteo(Vector<Unit> monList) {
-		target.curhp -= power *2;
-		System.out.printf("[%s]의 스킬 발동!\n", name);
-		System.out.printf("[%s]이 [%s]에게 %d의 데미지를 두번 입힙니다.\n", name, target.name, power);
-		if(target.curhp <= 0) {
-			System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
-			target.curhp = 0;
+	private void meteo(Vector<Unit> list) {
+		int cnt = 0;
+		while(true) {
+			Unit target = list.get(cnt ++);
+			if(target.getCurhp() > 0) {
+				target.curhp -= (power / 5 * 3);
+				if(target.curhp <= 0) {
+					System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
+					target.curhp = 0;
+				}
+			}
+			
+			if(cnt == list.size())
+				break;
+			
 		}
+		System.out.printf("[%s]의 스킬 발동!\n", name);
+		System.out.printf("[%s]이 적 전체에게 POWER의 60프로의 힘으로 메테오를 날립니다.\n", name);
 	}
+	
+	
 	
 }
