df10faeaed09695fe42cc2f0eee3138c71148b06
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-02 11:45:19 +0900

    add Player : 생성자 추가

---
+++ playGame/src/playGame/Player.java
@@ -0,0 +1,14 @@
+package playGame;
+
+public class Player extends Unit {
+	
+	public Player(String name, int maxHp, int power) {
+		super(name, maxHp, power);
+	}
+
+	@Override
+	public void skill(Unit target) {
+		
+	}
+	
+}
