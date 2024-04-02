b7f0f5380659b97c4baef4901b7f39f88785ec78
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-02 12:26:14 +0900

    add GameManager : 멤버 변수 추가

---
+++ playGame/src/playGame/GameManager.java
@@ -0,0 +1,16 @@
+package playGame;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameManager {
+	private Random ran = new Random();
+	public static Scanner scan = new Scanner(System.in);
+	public static String nextStage = "";
+	private String curStage = "";
+	
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
+	
+}
