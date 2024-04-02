09a40ce8e497c885316dcb1efc3037a532c22c34
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-02 14:36:59 +0900

    add Unit : printData 메소드 추가

--- playGame/src/playGame/Unit.java
+++ playGame/src/playGame/Unit.java
@@ -1,10 +1,10 @@
 package playGame;
 
 public abstract class Unit {
-	private int hp;
-	private int maxHp;
+	private int curhp;
+	private int maxhp;
 	private int power;
-	private String name;
+	protected String name;
 	private String state = "노말";
 	
 	public Unit() {
@@ -13,24 +13,29 @@ public abstract class Unit {
 	
 	public Unit(String name, int maxHp, int power) {
 		this.name = name;
-		this.hp = maxHp;
-		this.maxHp = maxHp;
+		this.curhp = maxHp;
+		this.maxhp = maxHp;
 		this.power = power;
 	}
 	
 	public void setMonster(int maxHp, int power) {
-		this.maxHp = maxHp;
+		this.maxhp = maxHp;
 		this.power = power;
 	}
 	
 	
+	
 	public void attack(Unit target) {
-		target.hp -= power;
+		target.curhp -= power;
 		System.out.printf("[%s]이 [%s]에게 %d의 데미지를 입힙니다.\n", name, target.name, power);
-		if(target.hp <= 0) {
+		if(target.curhp <= 0) {
 			System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.", name, target.name);
-			target.hp = 0;
+			target.curhp = 0;
 		}
+	}
+	
+	public void printData() {
+		System.out.printf("[%s][%d/%d][%d]", name, curhp, maxhp, power);
 	}
 	
 	public abstract void skill(Unit target);
