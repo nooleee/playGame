14ba2162c1a0dcd4bb4b47a2db3537f6700d4d4c
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-03 10:22:53 +0900

    modify UnitBat : skill 메소드 수정

--- playGame/src/playGame/UnitBat.java
+++ playGame/src/playGame/UnitBat.java
@@ -16,6 +16,7 @@ public class UnitBat extends Unit {
 		Unit target = playerList.get(idx);
 		if(target.getCurhp() > 0) {
 			target.curhp -= power;
+			this.curhp += power / 2;
 			System.out.printf("[%s]의 스킬 발동!\n", name);
 			System.out.printf("[%s]가 [%s]에게 %d만큼의 피해를 입히고 피해량의 절반을 회복합니다.", name, target.name, power);
 			if(target.curhp <= 0) {
