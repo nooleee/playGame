5a1ffc6732643ca7d8f671f3f62118338a0047a8
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-03 10:15:57 +0900

    modify Player : superAttack 메소드 수정

--- playGame/src/playGame/Player.java
+++ playGame/src/playGame/Player.java
@@ -27,18 +27,15 @@ public class Player extends Unit {
 	}
 	
 	private void superAttack(Vector<Unit> list) {
-		while(true) {
-			int idx = ran.nextInt(list.size());
-			Unit target = list.get(idx);
-			if(target.getCurhp() > 0) {
-				target.curhp -= power *2;
-				System.out.printf("[%s]의 스킬 발동!\n", name);
-				System.out.printf("[%s]이 [%s]에게 %d의 데미지를 두번 입힙니다.\n", name, target.name, power);
-				if(target.curhp <= 0) {
-					System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
-					target.curhp = 0;
-				}
-				break;
+		int idx = ran.nextInt(list.size());
+		Unit target = list.get(idx);
+		if(target.getCurhp() > 0) {
+			target.curhp -= power *2;
+			System.out.printf("[%s]의 스킬 발동!\n", name);
+			System.out.printf("[%s]이 [%s]에게 %d의 데미지를 두번 입힙니다.\n", name, target.name, power);
+			if(target.curhp <= 0) {
+				System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
+				target.curhp = 0;
 			}
 		}
 	}
