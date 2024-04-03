c25d797a2b1aeb0ab6d2b079b5286af48760f9b7
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-03 10:23:46 +0900

    add UnitOrc : skill 메소드 추가

--- playGame/src/playGame/UnitOrc.java
+++ playGame/src/playGame/UnitOrc.java
@@ -1,8 +1,10 @@
 package playGame;
 
+import java.util.Random;
 import java.util.Vector;
 
 public class UnitOrc extends Unit {
+	private Random ran = new Random();
 	
 	public UnitOrc() {
 		name = "오크";
@@ -10,8 +12,17 @@ public class UnitOrc extends Unit {
 
 	@Override
 	public void skill(Vector<Unit> monList, Vector<Player> playerList) {
-		System.out.println("한명에게 2배의 데미지 + 기절");
-		
+		int idx = ran.nextInt(playerList.size());
+		Unit target = playerList.get(idx);
+		if(target.getCurhp() > 0) {
+			target.curhp -= power * 2;
+			System.out.printf("[%s]의 스킬 발동!\n", name);
+			System.out.printf("[%s]가 [%s]에게 %d의 두배만큼 피해를 입힙니다.", name, target.name, power);
+			if(target.curhp <= 0) {
+				System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
+				target.curhp = 0;
+			}
+		}
 	}
 
 }
