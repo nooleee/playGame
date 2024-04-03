eee9a01a037819a17c62a7a9345d30794c7f6329
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-03 10:25:28 +0900

    add UnitWolf : skill 메소드 추가

--- playGame/src/playGame/UnitWolf.java
+++ playGame/src/playGame/UnitWolf.java
@@ -10,8 +10,23 @@ public class UnitWolf extends Unit {
 
 	@Override
 	public void skill(Vector<Unit> monList, Vector<Player> playerList) {
-		System.out.println("적 전체에게 공격력의 절반 데미지");
-		
+		int cnt = 0;
+		while(true) {
+			Unit target = playerList.get(cnt ++);
+			if(target.getCurhp() > 0) {
+				target.curhp -= (power / 2);
+				if(target.curhp <= 0) {
+					System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
+					target.curhp = 0;
+				}
+			}
+			
+			if(cnt == playerList.size())
+				break;
+			
+		}
+		System.out.printf("[%s]의 스킬 발동!\n", name);
+		System.out.printf("[%s]이 적 전체에게 POWER의 절반의 힘으로 할퀴기 공격을 합니다.\n", name);
 	}
 
 }
