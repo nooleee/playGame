0906e5709f1507014d19f96889cca5df4202b5fd
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-03 10:20:42 +0900

    add UnitBat : skill 메소드 추가

--- playGame/src/playGame/UnitBat.java
+++ playGame/src/playGame/UnitBat.java
@@ -1,8 +1,10 @@
 package playGame;
 
+import java.util.Random;
 import java.util.Vector;
 
 public class UnitBat extends Unit {
+	private Random ran = new Random();
 	
 	public UnitBat() {
 		name = "박쥐";
@@ -10,8 +12,17 @@ public class UnitBat extends Unit {
 
 	@Override
 	public void skill(Vector<Unit> monList, Vector<Player> playerList) {
-		System.out.println("적 한명에게 침묵 시전");
-		
+		int idx = ran.nextInt(playerList.size());
+		Unit target = playerList.get(idx);
+		if(target.getCurhp() > 0) {
+			target.curhp -= power;
+			System.out.printf("[%s]의 스킬 발동!\n", name);
+			System.out.printf("[%s]가 [%s]에게 %d만큼의 피해를 입히고 피해량의 절반을 회복합니다.", name, target.name, power);
+			if(target.curhp <= 0) {
+				System.out.printf("[%s]이 [%s]을 쓰러뜨렸습니다.\n", name, target.name);
+				target.curhp = 0;
+			}
+		}
 	}
 
 }
