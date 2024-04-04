310d5fe53e15945ccc5dc56fc2735bfab0fcb090
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-04 11:50:41 +0900

    add Guild : setGuild 메소드 추가

--- playGame/src/playGame/Guild.java
+++ playGame/src/playGame/Guild.java
@@ -10,6 +10,21 @@ public class Guild {
 	private Unit[] partyList;
 	
 	private void setGuild() {
-//		guildList.add(new Warrior("전사", 5, 1000, 45, 45, 0));
+		guildList.add(new Warrior("전사", 5, 1000, 45, 45, 0));
+		guildList.add(new Wizard("마법사", 8, 800, 60, 30, 0));
+		guildList.add(new Healer("힐러", 3, 500, 70, 30, 0));
+		
+		for(int i = 0; i < SIZE; i++) {
+			guildList.get(i).party = true;
+		}
+		
+		partyList = new Unit[SIZE];
+		
+		int cnt = 0;
+		for(int i = 0 ; i < guildList.size(); i++) {
+			Player target = guildList.get(i);
+			if(target.party) 
+				partyList[cnt ++] = target;
+		}
 	}
 }
