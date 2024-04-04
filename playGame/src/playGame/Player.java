acd11508d7cff8338aba2b57b07c8dd0ffdda676
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-04 17:10:02 +0900

    add Player : 길드와 인벤토리 메뉴 및 리스트, 사이즈 메소드 추가

--- playGame/src/playGame/Player.java
+++ playGame/src/playGame/Player.java
@@ -1,16 +1,52 @@
 package playGame;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public abstract class Player extends Unit {
 	
-	public Player(String name, int maxHp, int power) {
-		super(name, maxHp, power);
+	public static int money;
+	
+	public static Guild guild = new Guild();
+	public static Inventory inven = new Inventory();
+	
+	public Player(String name, int level,  int maxHp, int power, int def, int exp) {
+		super(name, level, maxHp, power, def, exp);
+	}
+	
+	public Player(String name, int level,  int maxHp, int power, int def, int exp, boolean party) {
+		super(name, level, maxHp, power, def, exp, party);
 	}
 	
 	public abstract void skill(Vector<Monster> monList, Vector<Player> playerList);
 
-	
+	  public void guildMenu() {
+	    guild.guildMenu();
+	  }
+
+	  public void inventoryMenu() {
+	    inven.inventoryMenu();
+	  }
+
+	  static public Vector<Player> getGuildList() {
+	    return guild.getGuildList();
+	  }
+
+	  static public ArrayList<Item> getItemList() {
+	    return inven.getItemList();
+	  }
+
+	  static public Player getGuildUnit(int num) {
+	    return guild.getGuildUnit(num);
+	  }
+
+	  static public int getGuildSize() {
+	    return guild.getGuildList().size();
+	  }
+
+	  static public int getItemSize() {
+	    return inven.getItemList().size();
+	  }
 	
 	
 }
