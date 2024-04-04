a1e71a12e81879d8290b7fe4d762a284914cf9a3
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-04 18:40:04 +0900

    add Shop : printItem 메소드 추가

--- playGame/src/playGame/Shop.java
+++ playGame/src/playGame/Shop.java
@@ -41,7 +41,40 @@ public class Shop {
 		temp = new Item();
 		temp.setItem(Item.RING, "다이아반지", 35, 20000);
 		itemList.add(temp);
-		
-		
 	}
+	
+	public void shopMenu() {
+		while(true) {
+			System.out.println("==========[상점]==========");
+			System.out.println("[1.무기] [2.방어구] [3.반지]");
+			int sel = GameManager.scan.nextInt() - 1;
+			
+			if(sel < 1 || sel > 3)
+				continue;
+			
+			if(sel == Item.WEAPON) 
+				System.out.println("==========[무기]==========");
+			else if(sel == Item.ARMOR) 
+				System.out.println("==========[방어구]==========");
+			else if(sel == Item.RING) 
+				System.out.println("==========[반지]==========");
+			
+			printItem(sel);
+		}
+	}
+	
+	private void printItem(int sel) {
+		int cnt = 0;
+		for(int i = 0; i < itemList.size(); i++) {
+			Item item = itemList.get(i);
+			if(item.getKind() == sel) {
+				System.out.printf("[%d번]\n", cnt+1);
+				System.out.printf("[이름 : %s] [능력 : %d] [가격 : %d]", item.getName(), item.getPower(), item.getPrice());
+				cnt++;
+			}
+			else
+				continue;
+		}
+	}
+	
 }
