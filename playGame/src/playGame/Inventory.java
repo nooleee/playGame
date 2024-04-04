9a5dcfcd57388ac4f590e3da6a40b2c77d1046ac
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-04 15:18:47 +0900

    add Inventory : 멤버변수 추가 및 inventoryMenu 메소드 추가

--- playGame/src/playGame/Inventory.java
+++ playGame/src/playGame/Inventory.java
@@ -1,5 +1,28 @@
 package playGame;
 
-public class Inventory {
+import java.util.ArrayList;
 
+public class Inventory {
+	private ArrayList<Item> itemList = new ArrayList<>();
+	
+	private final int WEAR = 1;
+	private final int SELL = 2;
+	
+	public void inventoryMenu() {
+		while (true) {
+			System.out.println("============ [인벤토리 메뉴] =============");
+			System.out.println("[1.착용] [2.판매] [0.뒤로가기]");
+			int sel = GameManager.scan.nextInt();
+			
+			if(sel < 0 || sel > 2)
+				return;
+			
+			if (sel == 0)
+				break;
+			if (sel == WEAR) {}
+//	          equipMenu();
+			else if (sel == SELL) {}
+//	          sellMenu();
+		}
+	}
 }
