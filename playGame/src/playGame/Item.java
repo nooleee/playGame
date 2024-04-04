0dede083076c4c145fae8a48581cbd23fd1efe29
nooleee <NOOL@DESKTOP-RADNT0G>
2024-04-04 15:10:11 +0900

    add Item : 멤버 변수 추가 및 생성자 추가

--- playGame/src/playGame/Item.java
+++ playGame/src/playGame/Item.java
@@ -1,5 +1,18 @@
 package playGame;
 
 public class Item {
-
+	public static final int WEAPON = 1;
+	public static final int ARMOR = 2; 
+	public static final int RING = 3; 
+	private int kind; 
+	private String name; 
+	private int power; 
+	private int price;
+	
+	public void setItem(int kind, String name, int power, int price) {
+		this.kind = kind;
+		this.name = name;
+		this.power = power;
+		this.price = price;
+	}
 }
